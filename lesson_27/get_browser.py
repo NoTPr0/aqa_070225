e30a82211d74eaa12850d2c263cecb1b08fa9aa2
NoTPr0 <vadeem.hacker@gmail.com>
2025-06-20 15:36:15 +0300

    test push

--- lesson_27/get_browser.py
+++ lesson_27/get_browser.py
@@ -10,13 +10,41 @@ def firefox(debug=False):
     Returns:
         WebDriver: Ініціалізований екземпляр Firefox WebDriver.
     """
+    geckodriver_path = "/snap/bin/geckodriver"
+    driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
+
     options = FirefoxOptions()
     if not debug:
         options.add_argument("--headless")  # Запуск у безголовному режимі (без UI)
 
-    driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox(service=driver_service)
     
     if debug:
         driver.maximize_window()  # Максимізація вікна лише якщо debug увімкнено
+
+    return driver
+
+
+class ChromeOptions:
+    pass
+
+
+def chrome(debug=False):
+    """Ініціалізує та повертає екземпляр Firefox WebDriver.
+
+    Args:
+        debug (bool): Якщо True, запускає браузер з вікном. Якщо False — у headless режимі.
+
+    Returns:
+        WebDriver: Ініціалізований екземпляр Firefox WebDriver.
+    """
+    options = ChromeOptions()
+    if not debug:
+        options.add_argument('--headless=new')  # Запуск у безголовному режимі (без UI)
+
+    driver = webdriver.Chrome(options=options)
+
+    # if debug:
+    driver.maximize_window()  # Максимізація вікна лише якщо debug увімкнено
 
     return driver
